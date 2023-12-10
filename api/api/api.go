1c71b6fc75811b0ac7b47e5efb10c25d83b172b5
Keoni Gandall <koeng101@gmail.com>
2023-12-10 12:08:28 -0800

    update templates

--- api/api/api.go
+++ api/api/api.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"embed"
 	_ "embed"
-	"encoding/json"
 	"fmt"
 	"html/template"
 	"io/fs"
@@ -39,7 +38,7 @@ var templates = template.Must(template.ParseFS(templatesFS, "templates/*"))
 
 // IndexHandler handles the basic HTML page for interacting with polyAPI.
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	err := templates.ExecuteTemplate(w, "chat.html", nil)
+	err := templates.ExecuteTemplate(w, "index.html", nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -82,21 +81,6 @@ func InitializeApp() App {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// Handle "hello" for testing with wasm
-	app.Router.HandleFunc("/hello", func(res http.ResponseWriter, req *http.Request) {
-		params := make(map[string]string)
-		if err := json.NewDecoder(req.Body).Decode(&params); err != nil {
-			panic(err)
-		}
-
-		res.Header().Add("Content-Type", "application/json")
-		if err := json.NewEncoder(res).Encode(map[string]string{
-			"message": fmt.Sprintf("Hello there %s!", params["name"]),
-		}); err != nil {
-			panic(err)
-		}
-	})
 
 	// Handle routes.
 	app.Router.HandleFunc("/", indexHandler)
