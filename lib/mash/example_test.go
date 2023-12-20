8e6f32d41b901f5eafb3f1907e67c745ffd3d63c
Koeng101 <Koeng101@gmail.com>
2023-12-20 08:50:53 -0800

    Removed murmur3 (#33)

--- lib/mash/example_test.go
+++ lib/mash/example_test.go
@@ -2,15 +2,16 @@ package mash_test
 
 import (
 	"fmt"
+	"hash/crc32"
 
 	"github.com/koeng101/dnadesign/lib/mash"
 )
 
 func ExampleMash() {
-	fingerprint1 := mash.New(17, 10)
+	fingerprint1 := mash.New(17, 10, crc32.NewIEEE())
 	fingerprint1.Sketch("ATGCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGA")
 
-	fingerprint2 := mash.New(17, 9)
+	fingerprint2 := mash.New(17, 9, crc32.NewIEEE())
 	fingerprint2.Sketch("ATGCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGATCGA")
 
 	distance := fingerprint1.Distance(fingerprint2)
