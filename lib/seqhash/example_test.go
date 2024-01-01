2203badce2f36d86ac6470c2f679c8af058c5299
Koeng101 <Koeng101@gmail.com>
2024-01-01 13:50:06 -0800

    Sam parser (#5)
    * minimally functioning sam parser
    
    * Update io/sam/sam.go
    
    Co-authored-by: Willow Carretero Chavez <sandiegobutterflies@gmail.com>
    
    * few udpates from comments
    
    * Update io/sam/sam.go
    
    Co-authored-by: Willow Carretero Chavez <sandiegobutterflies@gmail.com>
    
    * Update io/sam/sam.go
    
    Co-authored-by: Willow Carretero Chavez <sandiegobutterflies@gmail.com>
    
    * add validate
    
    * Update io/sam/sam.go
    
    Co-authored-by: Willow Carretero Chavez <sandiegobutterflies@gmail.com>
    
    * fixed sam_test
    
    * Add in bio parser
    
    * Updated sam to be in bio
    
    * add package level docs
    
    * Wrote WriteTo, still need testing
    
    * make linter happy
    
    * Noted Validate as not implemented
    
    * add validate function
    
    * fully test validate functions
    
    * fixed mapq and added more coverage
    
    ---------
    
    Co-authored-by: Willow Carretero Chavez <sandiegobutterflies@gmail.com>

--- lib/seqhash/example_test.go
+++ lib/seqhash/example_test.go
@@ -23,7 +23,7 @@ func Example_basic() {
 func ExampleRotateSequence() {
 	file, _ := os.Open("../data/puc19.gbk")
 	defer file.Close()
-	parser, _ := bio.NewGenbankParser(file)
+	parser := bio.NewGenbankParser(file)
 	sequence, _ := parser.Next()
 
 	sequenceLength := len(sequence.Sequence)
