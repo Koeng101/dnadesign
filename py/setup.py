d3ef1e27e5ccd15168b9da42a308afb3e4de510b
Keoni Gandall <koeng101@gmail.com>
2024-08-11 19:41:22 +0000

    build!

--- py/setup.py
+++ py/setup.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import platform
 from packaging import tags
 from setuptools import setup, find_packages
@@ -12,22 +11,6 @@ def get_shared_lib_ext():
     else:
         return ".so"
 
-def get_platform_tag():
-    # Get the most specific tag for this platform
-    tag = next(tags.sys_tags())
-    
-    if sys.platform.startswith('linux'):
-        return f'manylinux_2_17_{platform.machine()}'
-    elif sys.platform.startswith('darwin'):
-        if platform.machine() == 'arm64':
-            return f'macosx_11_0_arm64'
-        else:
-            return f'macosx_10_9_x86_64'
-    elif sys.platform.startswith('win'):
-        return f'win_amd64'
-    else:
-        return 'any'
-
 setup(
     name='dnadesign',
     version='0.1.2',
@@ -39,8 +22,6 @@ setup(
     setup_requires=[
         "cffi>=1.0.0",
     ],
-
-    plat_name=get_platform_tag(),
     include_package_data=True,
     zip_safe=False,
     author='Keoni Gandall',
