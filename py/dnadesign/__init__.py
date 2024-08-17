d1b4c0b0b24f95493142783772e22b052573c3b3
Keoni Gandall <koeng101@gmail.com>
2024-08-17 09:13:53 -0700

    add docs

---
+++ py/dnadesign/__init__.py
@@ -0,0 +1,8 @@
+r'''
+# What is dnadesign
+
+dnadesign is a batteries-included software suite for engineering biology.
+
+We're building a practical, modern, and ambitious library for engineering biology. While the library itself is written in Go, specifically for its simplicity and maintainability, it is accessible using Python, the programming language that biologists would be most familiar with.
+
+'''
