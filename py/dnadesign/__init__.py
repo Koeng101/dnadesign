7dea5c8e2280db2a284ac08ab6de25e49204f736
Keoni Gandall <koeng101@gmail.com>
2024-08-17 17:00:03 +0000

    add __all__

--- py/dnadesign/__init__.py
+++ py/dnadesign/__init__.py
@@ -6,3 +6,9 @@ dnadesign is a batteries-included software suite for engineering biology.
 We're building a practical, modern, and ambitious library for engineering biology. While the library itself is written in Go, specifically for its simplicity and maintainability, it is accessible using Python, the programming language that biologists would be most familiar with.
 
 '''
+
+# Required so that pdoc doesn't attempt to parse libdnadesign
+__all__ = [
+    "cffi_bindings",
+    "parsers"
+    ]
